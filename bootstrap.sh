7b9415edf1c72d5769ab01549dd7cd245d77b02c
Ryan Grove <ryan@wonko.com>
2012-09-08 12:47:04 -0700

    Make bootstrap.sh easier to re-run.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,14 @@ cd "$(dirname "$0")"
 git pull
 
 for file in .*; do
-    [ -f "$file" ] && [ -r "$file" ] && ln -s ~/dotfiles/$file ~/$file
+    if [ -f $file ] && [ -r $file ]; then
+        if [ -f ~/$file ] && [ ! -h ~/$file ]; then
+            echo "~/$file exists; creating a backup"
+            mv ~/$file ~/$file.bak || (echo "error creating backup"; exit 1)
+        fi
+
+        ln -sf ~/dotfiles/$file ~/$file
+    fi
 done
 unset file
 
